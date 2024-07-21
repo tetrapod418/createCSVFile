0f5deeb55057e0ed8d056a7c51421e83760c1187
tetrapod418 <hikeybow@gmail.com>
2024-07-21 14:28:40 +0900

    first commit

--- README.md
+++ README.md
@@ -1,1 +1,17 @@
 # createCSVFile
+
+## createCSV.goについて
+読み込んだCSVファイル内の{keyword}文字列を、コマンドライン引数を反映した結果をもとに置換して、指定ファイルに保存します。
+置換ルールは固定なので、下記以外の置換が必要な場合は、修正が必要です。
+|キー|置換ルール|補足|
+| --- | --- | --- |
+|title|第2引数|イベントタイトル。空白が含まれている場合は、""で囲む。例）"Cybozu Frontend Monthly"|
+|connpass|第3引数|イベントURL|
+|eventdate|第4引数|開催日付。年4桁/月2桁/日2桁形式|
+|preparelimit|eventdateの4日前（eventdateをもとに算出）||
+|postproc|eventdateの5日後（eventdateをもとに算出）||
+
+## 使い方
+コマンドライン引数で、置換用の文字列と、保存先のファイル名を指定します。
+プログラムと同じパスに、置換元ファイルrecords.csvが必要です。
+go run createCSV.go イベントタイトル（空白が含まれる場合は、""で囲む） エベントURL 開催日付（yyyy/MM/dd形式）置換結果の保存先ファイル名
---
+++ createCSV.go
@@ -0,0 +1,134 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"strings"
+	"strconv"
+	"time"
+	"log"
+	"errors"
+)
+
+func main() {
+
+	// get Args parameters
+	title, connpass, eventdate, preparelimit, postproc, resultpath, err := getArgs()
+	if (err != nil){
+		fmt.Println(err)
+		return
+	}
+	fmt.Printf("\n=== パラメータ ===\ntitle:%s\nconnpass:%s\ndate:%s\nprepare:%s\npostProc:%s\nresult:%s\n",
+	 title,connpass,eventdate,preparelimit,postproc, resultpath)
+
+	// get source csv data
+	str := getSourceCSV()
+
+	// replace parameters
+	str = setParameters(str, title, connpass, eventdate, preparelimit, postproc)
+
+	// csv書き出し
+	createNewTaskCSV(str, resultpath)
+}
+
+func getArgs()(title, connpass, eventdate, preparelimit, postproc, resultpath string, err error){
+	// get args
+	if(len(os.Args) != 5){
+		err = errors.New("パラメータに誤りがあります。\nUsage:\ngo run createCSV.go [event title] [connpass url] [event date] [result path]")
+		return title, connpass, eventdate, preparelimit, postproc, resultpath, err
+	}
+
+	// title
+	title = os.Args[1]
+	// url
+	connpass = os.Args[2]
+	// eventdate
+	date := strings.Split(os.Args[3], "/")
+	year := date[0]
+	month := date[1]
+	day :=date[2]
+
+	eventdate = os.Args[3]
+
+	// result path(output)
+	resultpath = os.Args[4]
+
+	// limit date for prepare
+	var preDate int
+	preDate, _ = strconv.Atoi(day)
+	preDate -=4
+	preparelimit = fmt.Sprintf("%s/%s/%d", year, month, preDate)
+	var postDate, _ = time.Parse("2006/01/02", os.Args[3])
+	y, m, d := postDate.AddDate(0,0,5).Date()
+	postproc = fmt.Sprintf("%d/%d/%d", y, m, d)
+
+	return title, connpass, eventdate, preparelimit,postproc,resultpath,err
+}
+
+// 雛形のCSV読み込み
+func getSourceCSV() (str string){
+	// open file
+	f, err := os.Open("./records.csv")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer f.Close()
+
+	// read file
+	buf := make([]byte, 1024)
+	count, err := f.Read(buf) // reader to buf
+	if err != nil {
+		log.Fatal(err)
+	}
+	str = string(buf[:count])
+	return str
+}
+
+func setParameters(str, title, connpass, eventdate, preparelimit, postproc string) (string){
+	// replace
+	// title置換
+	str = strings.Replace(str, "{title}", title, -1)
+	// connpass URL置換
+	str = strings.Replace(str, "{connpass}", connpass, -1)
+	// 開催日付
+	str = strings.Replace(str, "{eventdate}", eventdate, -1)
+	// 準備期限
+	str = strings.Replace(str, "{preparelimit}", preparelimit, -1)
+	// 開催後のCybozuTech動画掲載目安日
+	str = strings.Replace(str, "{postproc}", postproc, -1)
+
+	// 結果を返却
+	return str
+}
+
+func createNewTaskCSV(str, resultpath string){
+	outputpath := resultpath
+	// 出力パスに拡張子があれば、そのまま。拡張子がなければ、".csv"を付加する
+	if( !strings.HasSuffix(resultpath, ".csv") ){
+		outputpath = fmt.Sprintf("./%s.csv", resultpath)
+	}
+
+	// 書き込み可としてファイルを開く
+	f, err := os.Create(outputpath)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	
+	defer func() {
+		err := f.Close()
+		if err != nil {
+			fmt.Println(err)
+		}
+	}()
+
+	// CSV書き出し
+	data := []byte(str)
+	count, err := f.Write(data) // buf to writer
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	fmt.Printf("write %d bytes\n", count)
+	fmt.Println(str)
+}
